7136b355a0b285f2f6fe5a23dd583028f2b478c3
alanscode <alnngyn@gmail.com>
2018-03-19 21:33:20 -0700

    test

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var game = new Phaser.Game(
   window.innerWidth - 25,
-  600,
+  window.innerHeight - 25,
   Phaser.CANVAS,
   "phaser-multiplayer-socketio",
   { preload: preload, create: create, update: update, render: render }
@@ -12,32 +12,62 @@ function preload() {
   game.load.image("background", "assets/tests/debug-grid-1920x1920.png");
   game.stage.disableVisibilityChange = true;
 }
-
 var sprite;
+var ships = {};
 var weapon;
 var cursors;
 var fireButton;
 var socket;
-var masterId;
+var gameInitialized = false;
+
+function emitPlayerShip() {
+  socket.emit("shipStatus", {
+    id: socket.id,
+    x: sprite.x,
+    y: sprite.y,
+    angle: sprite.angle,
+    angularVelocity: sprite.body.angularVelocity,
+    acceleration: sprite.body.acceleration,
+    fire: fireButton.isDown
+  });
+}
+
+function renderShip(key) {
+  let ship = game.add.sprite(400, 300, "ship");
+
+  ship.anchor.set(0.5);
+
+  ship.scale.set(2, 2);
+
+  game.physics.arcade.enable(ship);
+
+  //sprite.body.drag.set(35);
+  ship.body.maxVelocity.set(300);
+  weaponEne = game.add.weapon(30, "bullet");
+
+  //  The bullets will be automatically killed when they are 2000ms old
+  weaponEne.bulletKillType = Phaser.Weapon.KILL_LIFESPAN;
+  weaponEne.bulletLifespan = 2000;
+
+  //  The speed at which the bullet is fired
+  weaponEne.bulletSpeed = 600;
+
+  //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
+  weaponEne.fireRate = 100;
+
+  //  Wrap bullets around the world bounds to the opposite side
+  weaponEne.bulletWorldWrap = false;
+  weaponEne.trackSprite(ship, 0, 0, true);
+
+  ship.weapon = weaponEne;
+  //  Tell the Weapon to track the 'player' Sprite
+  ships[key] = ship;
+
+  return ship;
+}
 
 function create() {
   socket = io("http://localhost:5000");
-
-  socket.on("ship", data => {
-    if (masterId != socket.id) {
-      console.log(data);
-      sprite.x = data.x;
-      sprite.y = data.y;
-      sprite.body.angularVelocity = data.angularVelocity;
-      sprite.angle = data.angle;
-      sprite.body.acceleration.set(data.acceleration);
-      sprite.body.velocity = data.velocity;
-
-      if (data.fire) {
-        weapon.fire();
-      }
-    }
-  });
 
   game.add.tileSprite(0, 0, 4920, 4920, "background");
 
@@ -81,57 +111,64 @@ function create() {
 
   fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
 
-  masterId = socket.id;
+  setInterval(emitPlayerShip, 1);
 
-  setInterval(emitShipSocket, 1);
-}
+  gameInitialized = true;
 
-function emitShipSocket() {
-  if (!masterId) return;
-  socket.emit("ship", {
-    id: socket.id,
-    x: sprite.x,
-    y: sprite.y,
-    angularVelocity: sprite.body.angularVelocity,
-    angle: sprite.angle,
-    acceleration: sprite.body.acceleration,
-    velocity: sprite.body.velocity,
-    fire: fireButton.isDown
+  socket.on("gameState", data => {
+    for (key in data.status) {
+      if (key != undefined && key != null && data.status[key].id != socket.id) {
+        //console.log(data.status[key])
+        if (!ships[key]) {
+          renderShip(key);
+        }
+        let playerShip = data.status[key];
+        ships[key].x = playerShip.x;
+        ships[key].y = playerShip.y;
+        ships[key].angle = playerShip.angle;
+        ships[key].body.angularVelocity = playerShip.angularVelocity;
+        ships[key].body.acceleration = playerShip.acceleration;
+        if (playerShip.fire) {
+          ships[key].weapon.fire();
+        }
+      }
+    }
   });
 }
 
 function update() {
-  if (cursors.up.isDown) {
-    game.physics.arcade.accelerationFromRotation(
-      sprite.rotation,
-      900,
-      sprite.body.acceleration
-    );
-    masterId = socket.id;
-  } else {
-    sprite.body.acceleration.set(0);
-  }
+  if (gameInitialized) {
+    if (cursors.up.isDown) {
+      game.physics.arcade.accelerationFromRotation(
+        sprite.rotation,
+        900,
+        sprite.body.acceleration
+      );
+      masterId = socket.id;
+    } else {
+      sprite.body.acceleration.set(0);
+    }
 
-  if (cursors.left.isDown) {
-    masterId = socket.id;
-    sprite.body.angularVelocity = -300;
-  } else if (cursors.right.isDown) {
-    masterId = socket.id;
-    sprite.body.angularVelocity = 300;
-  } else {
-    sprite.body.angularVelocity = 0;
-  }
+    if (cursors.left.isDown) {
+      masterId = socket.id;
+      sprite.body.angularVelocity = -300;
+    } else if (cursors.right.isDown) {
+      masterId = socket.id;
+      sprite.body.angularVelocity = 300;
+    } else {
+      sprite.body.angularVelocity = 0;
+    }
 
-  if (fireButton.isDown) {
-    masterId = socket.id;
-    weapon.fire();
-    //socket.emit("fire", socket.id);
-  }
+    if (fireButton.isDown) {
+      weapon.fire();
+    }
 
-  game.world.wrap(sprite, 16);
+    game.world.wrap(sprite, 16);
+  }
 }
 
 function render() {
-  weapon.debug();
-  game.debug.spriteInfo(sprite, 32, 32);
+  if (gameInitialized) {
+    game.debug.spriteInfo(sprite, 32, 32);
+  }
 }
--- server.js
+++ server.js
@@ -1,30 +1,35 @@
 const socket = require("socket.io");
 const express = require("express");
-var path = require('path');
+var path = require("path");
 
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + "/"));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
-app.listen(4000,()=>{
-    console.log("<<< Static Host listening on port 4000 >>>");  
-})
+app.listen(4000, () => {
+  console.log("<<< Static Host listening on port 4000 >>>");
+});
 
 var server = app.listen(5000, () => {
   console.log("<<< WS listening on port 5000 >>>");
 });
 
 var io = socket(server);
-
+const ships = {};
 io.on("connection", socket => {
   console.log("socket connection made", socket.id);
 
-  socket.on("ship", data => {
-    io.sockets.emit("ship", data);
-    console.log(data);
+  socket.on("shipStatus", data => {
+    ships[data.id] = data;
+
+    io.sockets.emit("gameState", { status: ships });
+    console.log(ships);
+  });
+
+  socket.on("disconnect", function() {
+    delete ships[socket.id];
   });
 });
-
